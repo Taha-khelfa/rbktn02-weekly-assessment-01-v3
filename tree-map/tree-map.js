ded5515e8ffd25a6193976931554af6e46e7a7bd
taha0505 <taha.khelfa@hotmail.fr>
2020-03-30 13:01:35 +0200

    assessement week2

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -1,23 +1,23 @@
 /**
-  *
-  * Implement a `map` method on this Tree class, using pseudoclassical instantiation.
-  *
-  * Map accepts a mapping function as its only argument. It traverses the tree,
-  * passing each node's value into the mapping function, and generates a new
-  * tree containing the results.
-  *
-  * So `map` should return a tree with the same structure, and different values,
-  * but it should NOT modify the tree that was passed in.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   var newTree = root1.map(function (value) {
+*
+* Implement a `map` method on this Tree class, using pseudoclassical instantiation.
+*
+* Map accepts a mapping function as its only argument. It traverses the tree,
+* passing each node's value into the mapping function, and generates a new
+* tree containing the results.
+*
+* So `map` should return a tree with the same structure, and different values,
+* but it should NOT modify the tree that was passed in.
+*
+* Example:
+*   var root1 = new Tree(1);
+*   var branch2 = root1.addChild(2);
+*   var branch3 = root1.addChild(3);
+*   var leaf4 = branch2.addChild(4);
+*   var leaf5 = branch2.addChild(5);
+*   var leaf6 = branch3.addChild(6);
+*   var leaf7 = branch3.addChild(7);
+*   var newTree = root1.map(function (value) {
   *     return value * 2;
   *   })
   *  newTree.value // 2
@@ -27,11 +27,24 @@
   *  newTree.children[1].children[1].value // 14
   *  root1.value // still 1
   */
-
-var Tree = function(value) {
-  this.value = value;
-  this.children = [];
-};
-
-
-
+  
+  var Tree = function(value) {
+    this.value = value;
+    this.children = [];
+  };
+  Tree.prototype.addChildren = function(value){
+    var newTree = new Tree(value)
+    this.children.push(newTree)
+  };
+  Tree.prototype.map =function(fn){
+    var newRoot = new Tree(fn(this.value));
+    if(this.children){
+      for(var i=0;i<this.children.length;i++){
+        newRoot.addChildren(this.children[i].map(fn))
+      }
+    }
+  };
+  
+  
+  
+  
